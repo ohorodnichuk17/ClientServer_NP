a17fd1fafefc9aa202850b25118d2b484164d2c1
PC10 <ogorodnicukulia@gmail.com>
2023-06-17 01:47:11 +0300

    Add project files.

---
+++ ClientServer_NP/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientServer_NP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string countryName = txtBoxCountryName.Text;
+
+            string result = SendRequestToServer(countryName);
+
+            MessageBox.Show($"Результат: {result}");
+        }
+
+        private string SendRequestToServer(string countryName)
+        {
+            string serverIP = "127.0.0.1";
+            int serverPort = 1234;
+
+            using (TcpClient client = new TcpClient(serverIP, serverPort))
+            {
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] buffer = Encoding.UTF8.GetBytes(countryName);
+                    stream.Write(buffer, 0, buffer.Length);
+
+                    byte[] responseBuffer = new byte[client.ReceiveBufferSize];
+                    int bytesRead = stream.Read(responseBuffer, 0, client.ReceiveBufferSize);
+                    string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+
+
+                    return response;
+                }
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+internal class Program
+{
+    private static Dictionary<string, string> countryCity = new Dictionary<string, string>()
+    {
+        {"Afghanistan", "Kabul"},
+        {"Albania", "Tirana"},
+        {"Algeria", "Algiers"},
+        {"Andorra", "Andorra la Vella"},
+        {"Angola", "Luanda"},
+        {"Argentina", "Buenos Aires"},
+        {"Australia", "Canberra"},
+        {"Austria", "Vienna"},
+        {"Belgium", "Brussels"},
+        {"Brazil", "Brasília"},
+        {"Canada", "Ottawa"},
+        {"China", "Beijing"},
+        {"Egypt", "Cairo"},
+        {"France", "Paris"},
+        {"Germany", "Berlin"},
+        {"India", "New Delhi"},
+        {"Italy", "Rome"},
+        {"Japan", "Tokyo"},
+        {"Mexico", "Mexico City"},
+        {"Russia", "Moscow"},
+        {"Spain", "Madrid"},
+        {"Ukraine", "Kyiv" },
+        {"United Kingdom", "London"},
+        {"United States", "Washington, D.C."}
+    };
+
+    static void Main(string[] args)
+    {
+        TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 1234);
+        listener.Start();
+
+        Console.WriteLine("Сервер запущено. Очікування підключень...");
+
+        while(true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Клієнт підключений!");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+            string country = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            string city = GetCityByCountry(country);
+
+            byte[] response = Encoding.UTF8.GetBytes(city);
+            stream.Write(response, 0, response.Length);
+
+            client.Close();
+        }
+
+        SaveCityCountryToJson(countryCity, "countryCity.json");
+    }
+
+    private static string GetCityByCountry(string cityCountry)
+    {
+        if(countryCity.ContainsKey(cityCountry))
+            return countryCity[cityCountry];
+
+        return string.Empty;
+    }
+
+    private static void SaveCityCountryToJson(Dictionary<string, string> cityCountry, string filePath)
+    {
+        string json = JsonConvert.SerializeObject(cityCountry, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(filePath, json);
+    }
+}
